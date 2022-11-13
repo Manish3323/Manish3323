855f04b30d8df79c58e60d8ece11af78b3f84b1a
manish3323 <gowardipemanish95@gmail.com>
2022-11-13 16:28:36 +0530

    update site with relevent data

---
+++ src/helper.ts
@@ -0,0 +1,8 @@
+export function dateDiffInYears(a, b) {
+  const _MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365;
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_YEAR);
+}
